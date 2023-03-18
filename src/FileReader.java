c76755349d2997abcb50a86bd17d8f9fdb2972bd
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2023-03-18 20:45:15 +0200

    operate function done

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,26 +1,9 @@
+
 package src;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
 
-public class FileReader  {
-    public static void main(String[] args) throws IOException {
-        try{
-            FileInputStream f = new FileInputStream("file10.txt");
-            DataInputStream d = new DataInputStream(f);
-            System.out.println(d.readUTF());
-            System.out.println(d.readUTF());
-            System.out.println(d.readUTF());
-//            System.out.println(d.readChar());
-            System.out.println(d.readUTF());
-
-        }catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-
-    }
+public class FileReader {
 }
